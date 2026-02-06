9d13cd0687a6fba66ad89d099c322d3423e1848d
Faina <113424218+faina4170@users.noreply.github.com>
2026-02-06 18:36:10 +0500

    Update task_3.py

--- leetcode/task_3.py
+++ leetcode/task_3.py
@@ -1,1 +1,15 @@
+# ПОИСК ВТОРОГО МАКСИМУМА
+n = int(input())
+scores = list(map(int, input().split()))
+scores = scores[:n]
 
+
+if scores:
+    unique_scores = list(set(scores))
+    if len(unique_scores) >= 2:
+        unique_scores.sort(reverse=True)
+        first_max = unique_scores[0]
+        second_max = unique_scores[1]
+        print(second_max)
+    else:
+        print(f"Все числа одинаковые: {unique_scores[0]}")
