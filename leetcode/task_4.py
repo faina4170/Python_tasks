f7e8f6b7134aef7e92e01db12f2059f79dae11a8
Faina <113424218+faina4170@users.noreply.github.com>
2026-02-06 18:37:03 +0500

    Update task_4.py

--- leetcode/task_4.py
+++ leetcode/task_4.py
@@ -1,1 +1,12 @@
+# ФАКТОРИАЛ ЧИСЛА
+def factorial(n):
+    if n < 0:
+        return "-1"
+    elif n == 0:
+        return 1
+    else:
+        result = 1
+    for i in range(1, n + 1):
+        result *= i
+    return result
 
